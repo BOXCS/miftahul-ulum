b9cda59f8a61ccded8e35154893b0804cb8f4d7e
rizki354-ok <rizkinasrulloh88@gmail.com>
2025-05-31 18:29:48 +0700

    designing data management pages

--- resources/views/management.blade.php
+++ resources/views/management.blade.php
@@ -2,18 +2,94 @@
 @push('styles')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.2.2/css/dataTables.bootstrap5.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+    <style>
+        .controls {
+            background: white;
+            padding: 1.5rem 2rem;
+            margin-top: 0 !important;
+        }
+
+        .header {
+            background: rgb(172, 199, 238);
+            padding: 2rem;
+            border-bottom: 1px solid rgb(235, 243, 255);
+            border-top-left-radius: 20px;
+            border-top-right-radius: 20px;
+        }
+
+        .main-title {
+            font-size: 2rem;
+            font-weight: 700;
+            color: black;
+            margin: 0;
+            text-align: center;
+        }
+
+        .subTitle {
+            color: #4f46e5;
+            font-weight: 600;
+        }
+
+        #datatable_wrapper .row:first-child {
+            display: none;
+        }
+
+        .dt-layout-table+.row {
+            padding: 20px;
+            padding-inline: 40px;
+            border-bottom-left-radius: 20px;
+            border-bottom-right-radius: 20px;
+        }
+
+        #datatable_wrapper {
+            margin-top: 0;
+            background: white;
+            border-top: 0.1px solid black;
+            border-bottom-left-radius: 20px;
+            border-bottom-right-radius: 20px;
+        }
+
+        .dt-layout-table {
+            margin-top: 0;
+            padding-block: 3px;
+        }
+    </style>
 @endpush
 @section('content')
     <div class="container-fluid">
         <div class="row g-4">
-            <h1>Manajemen Data <span class="subTitle">Santri</span></h1>
-            <select class="form-select" aria-label="Default select example" name="mode" id="mode" oninput="changeMode()">
-                <option value="santri">Santri</option>
-                <option value="ortu">Orang tua</option>
-                <option value="staf">Staf</option>
-            </select>
-            <a href="javascript:void(0)" class="btn btn-primary" id="add-button" data-bs-toggle="modal" data-bs-target="#TambahSantri">Tambah Santri Baru</a>
-            <div class="modal fade" id="TambahSantri" tabindex="-1" aria-labelledby="Tambah santri" aria-hidden="true">
+            <div class="header">
+                <h1 class="main-title">
+                    Manajemen Data <span class="subTitle">Santri</span>
+                </h1>
+            </div>
+            <div class="controls">
+                <div class="row g-3 align-items-center">
+                    <div class="col-md-3">
+                        <select class="form-select" name="mode" id="mode" oninput="changeMode()">
+                            <option value="santri">Santri</option>
+                            <option value="ortu">Orang Tua</option>
+                            <option value="staf">Staf</option>
+                        </select>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="input-group p-0">
+                            <input type="text" class="form-control" id="myInput" placeholder="Cari...">
+                            <span class="input-group-text">
+                                <i class="bi bi-search"></i>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <button class="btn btn-primary w-100" data-bs-toggle="modal" data-bs-target="#TambahSantri" id="add-button">
+                            <i class="bi bi-plus-circle me-1"></i>
+                            Tambah Santri Baru
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="modal fade" id="TambahSantri" tabindex="-1" aria-labelledby="Tambah santri">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -31,7 +107,7 @@
                     </div>
                 </div>
             </div>
-            <table id="datatable" class="table table-striped overflow-y" style="width:100%">
+            <table id="datatable" class="table row-border bg-light">
                 <thead>
                 </thead>
                 <tbody>
@@ -60,7 +136,7 @@
                 { title: 'Orang Tua', data: 'ortu.nama_lengkap' },
                 { title: 'Sidik Jari', data: 'sidik_jari' },
                 { title: 'Status', data: 'status' },
-                { title: 'Aksi', data: 'id_santri',
+                { title: 'Aksi', data: 'id_santri', width: '200px',
                     
                     // add button edit and delete
                     render: function (data, type, row) {
@@ -95,16 +171,21 @@
             ],
             // detail properti tiap kolom
             columnDefs: [
+                { className: "dt-head-center align-middle", targets: '_all' },
+                { targets: 0, className: 'dt-center' },
                 { targets: 3, orderable: false, searchable: false },
                 { targets: 4, orderable: false },
-                { targets: 5, orderable: false, searchable: false },
+                { targets: 5, orderable: false, searchable: false, className: 'dt-center' },
             ]
+        });
+        document.getElementById('myInput').addEventListener('keyup', function () {
+            tableObj.search(this.value).draw();
         });
         // function for changes datatable will be display
         function changeMode() {
             var text = document.getElementById('mode').value;
             var addButton = document.getElementById('add-button');
-            var subTitle =document.getElementsByClassName('subTitle')[0];
+            var subTitle = document.getElementsByClassName('subTitle')[0];
 
             // function for clear table (before reinitialize)
             var tableId = "#datatable";
@@ -113,16 +194,16 @@
                 tableObj.clear();
                 tableObj.destroy();
             }
-            
+
             //2nd empty html
             $(tableId + " tbody").empty();
             $(tableId + " thead").empty();
-            
+
             //3rd reCreate Datatable object
             switch (text) {
                 case "santri":
                     // ganti tombol "tambah"
-                    addButton.innerHTML = "Tambah Santri Baru";
+                    addButton.innerHTML = `<i class="bi bi-plus-circle me-1"></i>Tambah Santri Baru`;
                     addButton.setAttribute("data-bs-toggle", "modal");
                     addButton.setAttribute("data-bs-target", "#TambahSantri");
                     subTitle.innerText = "Santri";
@@ -136,7 +217,7 @@
                             { title: 'Orang Tua', data: 'ortu.nama_lengkap' },
                             { title: 'Sidik Jari', data: 'sidik_jari' },
                             { title: 'Status', data: 'status' },
-                            { title: 'Aksi', data: 'id_santri',
+                            { title: 'Aksi', data: 'id_santri', width: '200px',
                                 
                                 // add button edit and delete
                                 render: function (data, type, row) {
@@ -170,15 +251,17 @@
                             }
                         ],
                         columnDefs: [
+                            { className: "dt-head-center align-middle", targets: '_all' },
+                            { targets: 0, className: 'dt-center' },
                             { targets: 3, orderable: false, searchable: false },
                             { targets: 4, orderable: false },
-                            { targets: 5, orderable: false, searchable: false },
+                            { targets: 5, orderable: false, searchable: false, className: 'dt-center' },
                         ]
                     });
                     break;
                 case "ortu":
                     // ganti tombol "tambah"
-                    addButton.innerHTML = "Tambah Orang Tua Baru";
+                    addButton.innerHTML = `<i class="bi bi-plus-circle me-1"></i>Tambah Orang Tua Baru`;
                     addButton.setAttribute('href', '{{ route("orangtua.create") }}');
                     addButton.removeAttribute("data-bs-toggle");
                     addButton.removeAttribute("data-bs-target");
@@ -193,7 +276,7 @@
                             { title: 'Alamat', data: 'alamat' },
                             { title: 'No. Telp', data: 'no_telp' },
                             { title: 'Nama Santri', data: 'santri[, ].nama' },
-                            { title: 'Aksi', data: 'id_ortu',
+                            { title: 'Aksi', data: 'id_ortu', width: '200px',
                                 
                                 // add button edit and delete
                                 render: function (data, type, row) {
@@ -227,14 +310,16 @@
                             }
                         ],
                         columnDefs: [
+                            { className: "dt-head-center align-middle", targets: '_all' },
+                            { targets: 0, className: 'dt-center' },
                             { targets: 3, orderable: false },
-                            { targets: 5, orderable: false, searchable: false },
+                            { targets: 5, orderable: false, searchable: false, className: 'dt-center' },
                         ]
                     });
                     break;
                 case 'staf':
                     // ganti tombol "tambah"
-                    addButton.innerHTML = "Tambah Staf Baru";
+                    addButton.innerHTML = `<i class="bi bi-plus-circle me-1"></i>Tambah Staf Baru`;
                     addButton.setAttribute('href', '{{ route("staff.create") }}');
                     addButton.removeAttribute("data-bs-toggle");
                     addButton.removeAttribute("data-bs-target");
@@ -249,7 +334,7 @@
                             { title: 'Alamat', data: 'alamat' },
                             { title: 'No. Telp', data: 'no_telp' },
                             { title: 'Jabatan', data: 'jabatan' },
-                            { title: 'Aksi', data: 'id_staf',
+                            { title: 'Aksi', data: 'id_staf', width: '200px',
                                 
                                 // add button edit and delete
                                 render: function (data, type, row) {
@@ -283,8 +368,10 @@
                             }
                         ],
                         columnDefs: [
+                            { className: "dt-head-center align-middle", targets: '_all' },
+                            { targets: 0, className: 'dt-center' },
                             { targets: 3, orderable: false },
-                            { targets: 5, orderable: false, searchable: false },
+                            { targets: 5, orderable: false, searchable: false, className: 'dt-center' },
                         ]
                     });
                     break;
