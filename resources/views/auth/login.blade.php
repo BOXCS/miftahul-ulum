bffc01718c6685421223a1d331ae4e6fab141a79
Zaky <zakyabdurt@gmail.com>
2026-05-20 13:18:47 +0700

    view: change icon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -618,7 +618,7 @@
 
             <p class="hero-desc">
                 Mencetak Generasi Rabbani Berakhlakul Karimah.<br>
-                Akses informasi
+                Akses informasi, perkembangan santri, dan komunikasi resmi dalam satu platform terpadu.
             </p>
         </div>
 
